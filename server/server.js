d118a452da5251d8a6194e77f7bc530de5e8a67f
David Agaybi <davidagaybi@gmail.com>
2021-10-23 20:05:26 -0400

    third commit ...handling a few websites with switch

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
+
 
 const server = http.createServer((req, res) => {
-  //   console.log("request made");
-  console.log("url: ", req.url, " method:", req.method);
+  
+  // lodash
+
+  const num = _.random(0, 20);
+  
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log('Hello');
+    
+  });
+
+  greet();
+  greet();
+  
 
   // set header content type
   res.setHeader("Content-Type", "text/html"); //can send text or html or other types of data
@@ -12,12 +27,20 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-us":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
   // send an html file
@@ -27,6 +50,7 @@ const server = http.createServer((req, res) => {
       res.end();
     } else {
       //res.write(data); // if you're using one piece of data
+
       res.end(data); // place it directly in end
     }
   });
